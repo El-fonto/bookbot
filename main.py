7b71adea2b9ba165adbea2f68b5e4df71c45b79f
Fonto <laotrafuente880@gmail.com>
2024-10-19 17:23:55 -0600

    count characters draft 1 added

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ def main():
     words = count_words(text)
     print(text)
     print(f"this is the # of words in {book_path}: {words}")
+    print(f"this are the characters:")
 
 def get_book_path(path):
     with open(path) as f:
@@ -12,6 +13,11 @@ def get_book_path(path):
 def count_words(text):
     words = text.split()
     return len(words)
+
+def count_characters(words):
+    characters = {}
+    lowered_text = words.lower()
+    # TODO - split the text into characters and count with if statements add one per encounter; if not add to dictionary with a key. Similar to grocery list
     
 
 
